5cf492b685d66a875fc3aee2374f8c51c36a95aa
Conrad Irwin <conrad.irwin@gmail.com>
2024-09-06 19:48:34 -0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Conrad Irwin on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
